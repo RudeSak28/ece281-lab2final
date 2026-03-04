8928776e9c7cf724fe6f34959159b51b94969108
RudeSak28 <C28Saketh.Rudraraju@users.noreply.github.com>
2026-03-03 18:52:26 -0700

    commit

---
+++ src/sevenseg_decoder_tb.vhd
@@ -0,0 +1,90 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/02/2026 08:39:05 PM
+-- Design Name: 
+-- Module Name: sevenseg_decoder_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sevenseg_decoder_tb is
+--  Port ( );
+end entity  sevenseg_decoder_tb;
+
+architecture Behavioral of sevenseg_decoder_tb is
+    component sevenseg_decoder is
+    Port  ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+end component sevenseg_decoder;
+ 
+signal w_sw     : std_logic_vector (3 downto 0);
+signal w_seg_n  : std_logic_vector(6 downto 0);
+
+begin
+    u_decoder : sevenseg_decoder
+        port map(
+            i_hex => w_sw,
+            o_seg_n => w_seg_n
+        );
+    test_process : process
+    begin
+        w_sw <= x"0"; wait for 10 ns;
+            assert w_seg_n = "1000000" report "error on 0" severity failure;
+        w_sw <= x"1"; wait for 10 ns;
+            assert w_seg_n = "1111001" report "error on 1" severity failure;
+        w_sw <= x"2"; wait for 10 ns;
+            assert w_seg_n = "0100100" report "error on 2" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_seg_n = "0110000" report "error on 3" severity failure;
+        w_sw <= x"4"; wait for 10 ns;
+            assert w_seg_n = "0011001" report "error on 4" severity failure;
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_seg_n = "0010010" report "error on 5" severity failure;
+        w_sw <= x"6"; wait for 10 ns;
+            assert w_seg_n = "0100001" report "error on 6" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_seg_n = "0010000" report "error on 7" severity failure;
+        w_sw <= x"8"; wait for 10 ns;
+            assert w_seg_n = "0000001" report "error on 8" severity failure;
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_seg_n = "0001101" report "error on 9" severity failure;
+        w_sw <= x"a"; wait for 10 ns;
+            assert w_seg_n = "0001001" report "error on a" severity failure;
+        w_sw <= x"b"; wait for 10 ns;
+            assert w_seg_n = "1100001" report "error on b" severity failure;
+        w_sw <= x"c"; wait for 10 ns;
+            assert w_seg_n = "1110011" report "error on c" severity failure;
+        w_sw <= x"d"; wait for 10 ns;
+            assert w_seg_n = "1000011" report "error on d" severity failure;
+        w_sw <= x"e"; wait for 10 ns;
+            assert w_seg_n = "0110001" report "error on e" severity failure;
+        w_sw <= x"f"; wait for 10 ns;
+            assert w_seg_n = "0111001" report "error on f" severity failure;
+        wait;
+end process test_process;
+
+end architecture Behavioral;
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -79,23 +79,38 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design
-
+    component sevenseg_decoder is
+    Port  ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    end component sevenseg_decoder;
 
   -- create wire to connect button to 7SD enable (active-low)
-
+    signal w_sw     : std_logic_vector (3 downto 0);
+    signal w_seg_n  : std_logic_vector(6 downto 0);
+    signal w_7SD_EN_n : std_logic;
   
 begin
 	-- PORT MAPS ----------------------------------------
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
-	
+	u_decoder : sevenseg_decoder
+        port map(
+            i_hex => w_sw,
+            o_seg_n => w_seg_n
+        );
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+	w_sw <= sw;
+	seg <= w_seg_n;
 	-- wire up active-low 7SD anode (active low) to button (active-high)
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
 	-----------------------------------------------------
-	
+	w_7SD_EN_n  <= not btnC;
+
+    an(0)   <= w_7SD_EN_n;
+    an(1)   <= '1';
+    an(2)   <= '1';
+    an(3)   <= '1';
 end top_basys3_arch;
